2e5e36c8092f738944d3ce55d23fd9d268998569
Fábio de Oliveira Sales <fabio.oliveira.sales@gmail.com>
2019-01-04 17:14:40 -0200

    salvando dados da cotegoria

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -41,6 +41,49 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
   ngAfterContentChecked() {
     this.setPageTitle();
   }
+
+  submitForm() {
+    this.submittingForm = true;
+
+    if (this.currentAction == "new") {
+      this.createCategory();
+    } else {
+      this.updateCategory();
+    }
+
+  }
+  private updateCategory(): any {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+    this.categoryService.update(category);
+    
+  }
+  private createCategory(): any {
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+    this.categoryService.create(category).subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    );
+  }
+  private actionsForError(error: any): void {
+    toastr.error("Ocorreu um erro ao processar a sua solicitação");
+    this.submittingForm = false;
+
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, tente mais tarde."];
+    }
+
+  }
+
+  private actionsForSuccess(category: Category): void {
+    toastr.success("Solicitação processada com sucesso!");
+    //recarregar compomentes -- redirect/reload compoment page
+    this.router.navigateByUrl("categories", { skipLocationChange: true }).then(
+      () => this.router.navigate(["categories", category.id, "edit"])
+    );
+  }
+
   private setPageTitle() {
     if (this.currentAction == "new")
       this.pageTitle = "Cadastro de Nova Categoria"
@@ -67,7 +110,7 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked {
   private buildCategoryForm(): any {
     this.categoryForm = this.formBuilder.group({
       id: [null],
-      name: [null, Validators.required, Validators.minLength(2)],
+      name: [null, [Validators.required, Validators.minLength(2)]],
       description: [null]
     });
   }
