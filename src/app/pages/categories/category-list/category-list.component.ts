7103f6f8187d11e1c4a00ea94b0f89962649ab2b
Fábio de Oliveira Sales <fabio.oliveira.sales@gmail.com>
2019-01-09 17:40:57 -0200

    criação da clase genérica de formulário

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { BaseResourceList } from '../../../shared/components/base-resource-list/base-resource-list.component';
 
 import { Category } from "../shared/category.model";
 import { CategoryService } from "../shared/category.service";
@@ -8,34 +10,10 @@ import { CategoryService } from "../shared/category.service";
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent implements OnInit {
-
-  categories: Category[] = [];
+export class CategoryListComponent extends BaseResourceList<Category>{
 
   constructor(private categoryService: CategoryService) {
-
-  }
-
-  ngOnInit() {
-    this.categoryService.getAll().subscribe(
-      categories => this.categories = categories,
-      error => alert('Erro ao carregar a lista')
-    );
-  }
-
-
-  deleteCategory(category: Category) {
-
-    const mustDelete = confirm('Deseja realmente excluir este item?') 
-
-    if(!mustDelete){
-      return;
-    }
-
-    this.categoryService.delete(category.id).subscribe(
-      () => this.categories = this.categories.filter(element => element != category),
-      () => console.log('error ao excluir')
-    );
+    super(categoryService);
   }
 
 }
