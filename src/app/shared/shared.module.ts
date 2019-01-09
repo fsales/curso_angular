e8ebbcefe4b4a9769e2a2f02cec91ce424392190
Fábio de Oliveira Sales <fabio.oliveira.sales@gmail.com>
2019-01-09 21:58:59 -0200

    criando form-field-error

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,9 +7,13 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { FormFieldErrorComponent } from './components/form-field-error/form-field-error.component';
+import { ServerErrorMessagesComponent } from './components/server-error-messages/server-error-messages.component';
 
 @NgModule({
-  declarations: [BreadCrumbComponent, PageHeaderComponent, FormFieldErrorComponent],
+  declarations: [BreadCrumbComponent,
+    PageHeaderComponent,
+    FormFieldErrorComponent,
+    ServerErrorMessagesComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
@@ -23,6 +27,7 @@ import { FormFieldErrorComponent } from './components/form-field-error/form-fiel
     BreadCrumbComponent,
     PageHeaderComponent,
     FormFieldErrorComponent,
+    ServerErrorMessagesComponent,
   ]
 })
 export class SharedModule { }
